fcee0a437f2decc240e687823c975b627c262daa
alexsteeel <rf-89@list.ru>
2020-04-18 19:26:41 +0300

    Add example method CreateExpressionBlocks.

--- ExpressionTreesExample/Program.cs
+++ ExpressionTreesExample/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,35 @@ namespace ExpressionTreesExample
     {
         static void Main(string[] args)
         {
+            CreateExpressionBlocks();
+
+            Console.ReadKey();
+        }
+
+        private static void CreateExpressionBlocks()
+        {
+            BlockExpression blockExpr = Expression.Block(
+                            Expression.Call(
+                                null,
+                                typeof(Console).GetMethod("Write", new Type[] { typeof(String) }),
+                                Expression.Constant("Hello ")
+                               ),
+                            Expression.Call(
+                                null,
+                                typeof(Console).GetMethod("WriteLine", new Type[] { typeof(String) }),
+                                Expression.Constant("World!")
+                                ),
+                            Expression.Constant(42)
+                        );
+
+            Console.WriteLine("The result of executing the expression tree:");        
+            var result = Expression.Lambda<Func<int>>(blockExpr).Compile()();
+
+            Console.WriteLine("The expressions from the block expression:");
+            foreach (var expr in blockExpr.Expressions)
+            {
+                Console.WriteLine(expr.ToString());
+            }                
         }
     }
 }
